dec4511dcf14c85f3d246cbb4d870dcd32585b78
Mohammed Yousuf <yousuf89786@gmail.com>
2025-09-25 02:29:39 +0530

    Update Main.java
    Added requirements

--- SnakeAndLadder/src/Main.java
+++ SnakeAndLadder/src/Main.java
@@ -3,6 +3,12 @@ import model.Game;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        //Requirements
+        //1)any number of players
+        //2)any number of dices
+        //3)size of board dynamic
+        //4)Only one winner
+        //5)We can select the number of snakes and ladders
         Game game = new Game();
         game.setBoard(36, 5, 5);
         game.setDice(2);
